65e56410a5fa480bba121e4059de217a5b56cd26
TANZZ-eng <ibnucd30@gmail.com>
2026-01-16 15:49:44 +0700

    Update token list

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8490948740:AAFnls4i9XrxCJT9ZyiZG7pSAc5V_F9Wc0E",
-    "8541979779:AAEzksMbOhrZ7Mu6WoK_ZodiGZjdQq_L2vY"
+    "8541979779:AAEzksMbOhrZ7Mu6WoK_ZodiGZjdQq_L2vY",
+    "8562874275:AAHgGvs610Njqe5lwvXyzGjxQdX2Aker5To"
   ]
 }
